2a2d76e3c20a8e6df22fba86081f6e5164798038
Paulo Solipa <psolipa@product-league.com>
2023-03-06 14:58:37 +0000

    Test

--- src/scripts/unzipAndCopyConfigurations.js
+++ src/scripts/unzipAndCopyConfigurations.js
@@ -4,9 +4,11 @@ var path = require("path");
 var AdmZip = require("adm-zip");
 
 var utils = require("./utilities");
+var fs = require('fs');
+const path = './google-services.json';
 
 var constants = {
-  notificareServices: "services"
+  googleServices: "google-services"
 };
 
 module.exports = function(context) {
@@ -25,46 +27,50 @@ module.exports = function(context) {
     utils.handleError("Invalid platform", defer);
   }
 
-  var wwwPath = utils.getResourcesFolderPath(context, platform, platformConfig);
-  var sourceFolderPath = utils.getSourceFolderPath(context, wwwPath);
-  var googleServicesZipFile = utils.getZipFile(sourceFolderPath, constants.notificareServices);
-  if (!googleServicesZipFile) {
-    throw new Error("No configuration zip file found (google-services-zip). You can check how to configure this file at: https://success.outsystems.com/Documentation/11/Extensibility_and_Integration/Mobile_Plugins/Firebase_Plugins");
-  }
+  if (fs.existsSync(path)) {
+    var wwwPath = utils.getResourcesFolderPath(context, platform, platformConfig);
+    var sourceFolderPath = utils.getSourceFolderPath(context, wwwPath);
+    var googleServicesZipFile = utils.getZipFile(sourceFolderPath, constants.googleServices);
+    if (!googleServicesZipFile) {
+      throw new Error("No configuration zip file found (google-services-zip). You can check how to configure this file at: https://success.outsystems.com/Documentation/11/Extensibility_and_Integration/Mobile_Plugins/Firebase_Plugins");
+    }
+  
+    var zip = new AdmZip(googleServicesZipFile);
 
-  var zip = new AdmZip(googleServicesZipFile);
+    var targetPath = path.join(wwwPath, constants.googleServices);
+    zip.extractAllTo(targetPath, true);
 
-  var targetPath = path.join(wwwPath, constants.notificareServices);
-  zip.extractAllTo(targetPath, true);
+    var files = utils.getFilesFromPath(targetPath);
+    if (!files) {
+      utils.handleError("No directory found", defer);
+    }
 
-  var files = utils.getFilesFromPath(targetPath);
-  if (!files) {
-    utils.handleError("No directory found", defer);
-  }
+    var fileName = files.find(function (name) {
+      return name.endsWith(platformConfig.firebaseFileExtension);
+    });
+    if (!fileName) {
+      utils.handleError("No file found", defer);
+    }
 
-  var fileName = files.find(function (name) {
-    return name.endsWith(platformConfig.firebaseFileExtension);
-  });
-  if (!fileName) {
-    utils.handleError("No file found", defer);
-  }
+    var sourceFilePath = path.join(targetPath, fileName);
+    var destFilePath = path.join(context.opts.plugin.dir, fileName);
 
-  var sourceFilePath = path.join(targetPath, fileName);
-  var destFilePath = path.join(context.opts.plugin.dir, fileName);
+    if(!utils.checkIfFolderExists(destFilePath)){
+      utils.copyFromSourceToDestPath(defer, sourceFilePath, destFilePath);
+    }
 
-  if(!utils.checkIfFolderExists(destFilePath)){
-    utils.copyFromSourceToDestPath(defer, sourceFilePath, destFilePath);
-  }
-
-  if (cordovaAbove7) {
-    var destPath = path.join(context.opts.projectRoot, "platforms", platform, "app");
-    if (utils.checkIfFolderExists(destPath)) {
-      var destFilePath = path.join(destPath, fileName);
-      if(!utils.checkIfFolderExists(destFilePath)){
-        utils.copyFromSourceToDestPath(defer, sourceFilePath, destFilePath);
+    if (cordovaAbove7) {
+      var destPath = path.join(context.opts.projectRoot, "platforms", platform, "app");
+      if (utils.checkIfFolderExists(destPath)) {
+        var destFilePath = path.join(destPath, fileName);
+        if(!utils.checkIfFolderExists(destFilePath)){
+          utils.copyFromSourceToDestPath(defer, sourceFilePath, destFilePath);
+        }
       }
     }
-  }
+  } 
+
+  
       
   return defer.promise;
 }
