43ed7627f892aad57c535272d68702339dcc3a4c
Paulo Solipa <psolipa@product-league.com>
2023-03-06 16:58:54 +0000

    Unzip notificare

--- src/scripts/unzipAndCopyNServices.js
+++ src/scripts/unzipAndCopyNServices.js
@@ -3,7 +3,7 @@
 var path = require("path");
 var AdmZip = require("adm-zip");
 
-var utils = require("./utilities");
+var utils = require("./utilitiesNotificare");
 
 var constants = {
   notificareServices: "notificare-services"
